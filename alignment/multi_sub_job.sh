c15f42ba5f6a835c7f07d577ac1483c27bdbdc10
Honza Petkov <141332109+petkis@users.noreply.github.com>
2026-05-05 18:49:12 +0200

    Update multi_sub_job.sh

--- alignment/multi_sub_job.sh
+++ alignment/multi_sub_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 while IFS= read -r chrom; do
-    qsub -v CHROM="${chrom}",TOOL="winnowmap" paternal_gen_job.pbs
+    qsub -v CHROM="${chrom}",TOOL="winnowmap" alignment_job.pbs
 done < chroms_list.txt
